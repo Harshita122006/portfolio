e1a2104a759b3c47b0e3bf96a9166698f8d62068
Harshita122006 <harshi.dm.12@gmail.com>
2026-03-08 16:56:03 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,11 +1,14 @@
 function toggleChat(){
+
 let chat = document.getElementById("chatbot");
 
-if(chat.style.display === "none"){
-chat.style.display = "flex";
-}else{
+if(chat.style.display === "flex"){
 chat.style.display = "none";
 }
+else{
+chat.style.display = "flex";
+}
+
 }
 
 function sendMessage(){
@@ -13,27 +16,27 @@ function sendMessage(){
 let input = document.getElementById("userInput").value.toLowerCase();
 let chatbox = document.getElementById("chatbox");
 
-chatbox.innerHTML += "<div class='user'>" + input + "</div>";
+chatbox.innerHTML += "<div class='user-msg'>" + input + "</div>";
 
-let response = "Sorry, I didn't understand that.";
+let response = "Sorry, I didn't understand.";
 
 if(input.includes("admission")){
-response = "You can apply through the Garden City University admission portal.";
-}
-
-else if(input.includes("course")){
-response = "We offer BCA, BBA, BCom and many postgraduate courses.";
-}
-
-else if(input.includes("fee")){
-response = "Fee details are available in the admissions section.";
+response = "You can apply through the university admission portal.";
 }
 
 else if(input.includes("hostel")){
 response = "Yes, hostel facilities are available for students.";
 }
 
-chatbox.innerHTML += "<div class='bot'>" + response + "</div>";
+else if(input.includes("fees")){
+response = "Fee details are available on the admissions page.";
+}
+
+else if(input.includes("course")){
+response = "We offer programs like BCA, BBA and BCom.";
+}
+
+chatbox.innerHTML += "<div class='bot-msg'>" + response + "</div>";
 
 document.getElementById("userInput").value="";
 
